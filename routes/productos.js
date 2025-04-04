9a62aa163e2c3ad60125d5d5ede5095d7250df14
GustavoDigitalHouse <gustavo_tavella@hotmail.com>
2025-04-04 20:29:01 -0300

    Armado nuevo con Express
    Se implementa el trabajo final con el uso de express

--- routes/productos.js
+++ routes/productos.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/register', function(req, res, next) {
-  res.render('register.ejs', { title: 'Express' });
+router.get('/detail', function(req, res, next) {
+  res.render('productDetail', { title: 'Express' });
 });
 
 module.exports = router;
